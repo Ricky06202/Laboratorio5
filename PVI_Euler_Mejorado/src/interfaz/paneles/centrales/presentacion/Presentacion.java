d3c75a275b84c4dea57cb0350973d859382345f9
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2022-11-26 17:52:02 -0500

    agregamos imagenes a la presentacion

--- PVI_Euler_Mejorado/src/interfaz/paneles/centrales/presentacion/Presentacion.java
+++ PVI_Euler_Mejorado/src/interfaz/paneles/centrales/presentacion/Presentacion.java
@@ -1,9 +1,13 @@
 package interfaz.paneles.centrales.presentacion;
 
+import interfaz.campos.Imagen;
 import interfaz.paneles.centrales.propiedades.Contenedor;
 
 public class Presentacion extends Contenedor {
     public Presentacion() {
         add(new TemaEIntegrantes());
+        int escala = 200;
+        add(new Imagen(10, 5, escala, escala, "src\\imagenes\\logoUTP.png"));
+        add(new Imagen(600-5, 5, escala, escala, "src\\imagenes\\logoSistemas.png"));
     }
 }
